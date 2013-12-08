ab78e0caa6a7332a8d80ae9448305fb3b989b3d3
Ray Grasso <ray.grasso@gmail.com>
2013-12-08 17:21:48 +0800

    Fix bad path

--- tools.rb
+++ tools.rb
@@ -1,7 +1,7 @@
 dep 's3cmd configured', :access_key, :secret_key do
   requires 's3cmd.managed'
 
-  met? { '/home/vagrant/.s3cfg'.p.exists? }
+  met? { '~/.s3cfg'.p.exists? }
   meet {
     render_erb 's3cmd/s3cfg.erb', :to => "~/.s3cfg", :perms => '600'
   }
