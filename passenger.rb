555f28d2887eceaf60d6d45cc8a9947edfdf455d
Ray Grasso <ray.grasso@gmail.com>
2014-02-25 22:00:14 +0800

    Extract version info from passenger deps

--- passenger.rb
+++ passenger.rb
@@ -1,39 +1,75 @@
-PASSENGER_VERSION = '4.0.23'
-
-dep 'passenger.gem' do
-  gem_name 'passenger'
-  ruby_version '2.0.0'
-  version PASSENGER_VERSION
-  requires '2.0.0.chruby'
-end
-
-
 dep 'passenger apache' do
-  requires 'passenger apache install',
-           'passenger apache config',
+  requires '2.1.1.passenger_apache',
            'apache module enabled'.with(module_name: 'rewrite')
 end
 
-dep 'passenger apache install' do
-  requires 'apache2-mpm-prefork.managed',
-           'apache2-prefork-dev.managed',
-           'passenger.gem'
+dep '2.1.1.passenger_apache' do
+  version('4.0.37')
+  ruby_version('2.1.1')
+end
 
-  met? { "/opt/rubies/2.0.0-p247/lib/ruby/gems/2.0.0/gems/passenger-#{PASSENGER_VERSION}/buildout/apache2/mod_passenger.so".p.exists? }
-  meet {
-    shell "chruby-exec 2.0.0 -- /opt/rubies/2.0.0-p247/bin/passenger-install-apache2-module --auto"
+meta 'passenger_apache' do
+  accepts_value_for :version, '4.0.37'
+  accepts_value_for :ruby_version, :basename
+
+  template {
+    def ruby_major_version
+      _, major = *ruby_version.match(/(\d+\.\d+\.)\d+/)
+      "#{major}0"
+    end
+
+    requires 'passenger_apache install'.with(version: version,
+                                    ruby_version: ruby_version,
+                                    ruby_major_version: ruby_major_version),
+            'passenger_apache config'.with(version: version,
+                                    ruby_version: ruby_version,
+                                    ruby_major_version: ruby_major_version)
   }
 end
 
-dep 'passenger apache config' do
-  requires 'passenger apache install'
+dep 'passenger_apache install', :version, :ruby_version, :ruby_major_version do
+  requires 'apache2-mpm-prefork.managed',
+           'apache2-prefork-dev.managed',
+           'gem'.with(gem_name: 'passenger',
+                      ruby_version: ruby_version,
+                      version: version)
 
-  met? { "/etc/apache2/apache2.conf".p.grep /LoadModule passenger_module/ }
+  met? { "/opt/rubies/#{ruby_version}/lib/ruby/gems/#{ruby_major_version}/gems/passenger-#{version}/buildout/apache2/mod_passenger.so".p.exists? }
   meet {
-    sudo "echo \"\n# Passenger config\" >> /etc/apache2/apache2.conf" and
-    sudo "echo \"LoadModule passenger_module /opt/rubies/2.0.0-p247/lib/ruby/gems/2.0.0/gems/passenger-#{PASSENGER_VERSION}/buildout/apache2/mod_passenger.so\" >> /etc/apache2/apache2.conf" and
-    sudo "echo \"PassengerRoot /opt/rubies/2.0.0-p247/lib/ruby/gems/2.0.0/gems/passenger-#{PASSENGER_VERSION}\" >> /etc/apache2/apache2.conf" and
-    sudo "echo \"PassengerDefaultRuby /opt/rubies/2.0.0-p247/bin/ruby\" >> /etc/apache2/apache2.conf"
+    shell "chruby-exec #{ruby_version} -- /opt/rubies/#{ruby_version}/bin/passenger-install-apache2-module --auto"
+  }
+end
+
+dep 'passenger_apache config', :version, :ruby_version, :ruby_major_version do
+  def config_file
+    "/etc/apache2/apache2.conf"
+  end
+
+  def config_text
+    <<-EOF
+
+# Passenger config for version #{version} on ruby #{ruby_version}
+LoadModule passenger_module /opt/rubies/#{ruby_version}/lib/ruby/gems/#{ruby_major_version}/gems/passenger-#{version}/buildout/apache2/mod_passenger.so
+<IfModule mod_passenger.c>
+  PassengerRoot /opt/rubies/#{ruby_version}/lib/ruby/gems/#{ruby_major_version}/gems/passenger-#{version}
+  PassengerDefaultRuby /opt/rubies/#{ruby_version}/bin/ruby
+</IfModule>
+# end Passenger config
+EOF
+  end
+
+  setup {
+    unmeetable! "This dep has to be run as root." unless shell('whoami') == 'root'
+  }
+
+  met? { config_file.p.read.include? config_text }
+
+  meet {
+    content = config_file.p.read
+    content.gsub! /# Passenger config for version.+# end Passenger config/m, ""
+    content += config_text
+
+    config_file.p.write content
   }
   after { sudo 'service apache2 restart' }
 end
