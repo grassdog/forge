4297a6c7cc303fe0a36788bb76048bd082e7d0a3
Ray Grasso <ray.grasso@gmail.com>
2014-08-10 12:18:53 +0800

    Remove chruby auto

--- chruby.rb
+++ chruby.rb
@@ -31,7 +31,6 @@ dep 'chruby-global' do
 [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ] || return
 
 source /usr/local/share/chruby/chruby.sh
-source /usr/local/share/chruby/auto.sh
 chruby $(cat ~/.ruby-version 2> /dev/null || echo '2.1.1')
 EOF
 
